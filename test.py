af52346bb95b1c486578db71c39b40dd4b430677
Antotech <33504941+Antotech@users.noreply.github.com>
2024-09-19 12:56:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,17 +4,12 @@ import tqdm
 from neurons.compare_solutions import compare
 
 if __name__ == '__main__':
-    data = [compare() for i in tqdm.tqdm(range(20))]
+    data = [compare(min_node=2000, max_node=5000) for i in tqdm.tqdm(range(3))]
     data = np.array(data)
 
-    print("BEAM:", data[:, 0].mean())
-    print("BASELINE:", data[:, 1].mean())
-    print("NNS_VALI :", data[:, 2].mean())
-    print("HPN:", data[:, 3].mean())
-    print("CHRIST:", data[:, 4].mean())
-    print("ENHANCED:", data[:, 5].mean())
-    print("OR_SOLVER:", data[:, 6].mean())
-    print("MIN:", data[:, 7].mean())
+    print("BASELINE:", data[:, 0].mean())
+    print("ANNEALER:", data[:, 1].mean())
+    print("MIN.    :", data[:, 2].mean())
 
 
 
