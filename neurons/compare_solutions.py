321e5560bc4ea52e88de23a6617901c5a808db01
Antotech <33504941+Antotech@users.noreply.github.com>
2024-09-20 13:06:46 +0300

    Update compare_solutions.py
    Integrated the solvers

--- neurons/compare_solutions.py
+++ neurons/compare_solutions.py
@@ -8,9 +8,10 @@ from pydantic import ValidationError
 
 from graphite.data import ASIA_MSB_DETAILS, load_dataset, WORLD_TSP_DETAILS
 from graphite.data.distance import geom_edges, euc_2d_edges, man_2d_edges
+from graphite.solvers.christofides import solve
 from graphite.protocol import GraphV2Synapse, GraphV2Problem
 from neurons.call_method import beam_solver_solution, baseline_solution, nns_vali_solver_solution, hpn_solver_solution, \
-    scoring_solution, tsp_annealer_solver
+    scoring_solution, enhanced_solver_solution, or_solver_solution
 
 loaded_datasets = {
     ASIA_MSB_DETAILS['ref_id']: load_dataset(ASIA_MSB_DETAILS['ref_id']),
@@ -56,25 +57,70 @@ def compare(min_node=2000, max_node=5000):
     synapse_request = generate_problem_from_dataset(min_node=min_node,max_node=max_node)
     edges = recreate_edges(synapse_request.problem)
     synapse_request.problem.edges = edges
-
-    t1 = time.time_ns()
+    t1 = time.time()
+    beam_synapse = asyncio.run(beam_solver_solution(synapse_request))
+    t2 = time.time()
     baseline_synapse = asyncio.run(baseline_solution(synapse_request))
-    t2 = time.time_ns()
-    annealer_synapse = asyncio.run(tsp_annealer_solver(synapse_request))
-    t3 = time.time_ns()
+    t3 = time.time()
+    nns_vali_synapse = asyncio.run(nns_vali_solver_solution(synapse_request))
+    t4 = time.time()
+    hpn_synapse = asyncio.run(hpn_solver_solution(synapse_request))
+    t5 = time.time()
+    christ_synapse = solve(synapse_request)
+    t6 = time.time()
+    enhanced_synapse = asyncio.run(enhanced_solver_solution(synapse_request))
+    t7 = time.time()
+    or_synapse = asyncio.run(or_solver_solution(synapse_request))
+    t8 = time.time()
+    d1 = t2 - t1
+    d2 = t3 - t2
+    d3 = t4 - t3
+    d4 = t5 - t4
+    d5 = t6 - t5
+    d6 = t7 - t5
+    d7 = t8 - t7
+    if d1 > 10 :
+        print(f"d1")
+        exit(1)
+    if d2 > 10 :
+        print(f"d2")
+        exit(1)
+    if d3 > 10 :
+        print(f"d3")
+        exit(1)
+    if d4 > 10 :
+        print(f"d4")
+        exit(1)
+    if d5 > 10 :
+        print(f"d5")
+        exit(1)
+    if d6 > 10 :
+        print(f"d6")
+    if d7 > 10 :
+        print(f"d7")       
 
-    print(
-        f'Computational time: {(t2 - t1) / 1e6} ms, time annealer: {(t3 - t2) / 1e6}, number of nodes: {synapse_request.problem.n_nodes}')
-    list_synapse = [baseline_synapse, annealer_synapse]
+
+    list_synapse = [beam_synapse, baseline_synapse,nns_vali_synapse,hpn_synapse,christ_synapse,enhanced_synapse,or_synapse]
     scores = [scoring_solution(synapse) for synapse in list_synapse]
 
-    min_score = min(scores)
+    min_score = min(scores)  # this give inacurrate result when any result is inf
     scores.append(min_score)
-
 
     return scores
 
 
-
 if __name__ == '__main__':
-    ...
+    # synapse_request = generate_problem()
+    # # print(f"synapse_request = {synapse_request}")
+    # json_data = json.dumps(synapse_request.problem.dict())
+    # print(f"synapse_request problem = {json_data}")
+    # graph_problem_instance = GraphProblem.parse_raw(json_data)
+    # print(f"GraphProblem instance: {isinstance(graph_problem_instance, GraphProblem)}")
+
+    # synapse = asyncio.run(beam_solver_solution(synapse_request))
+    # print(f"route = {synapse.solution}  length = {len(synapse.solution)}")
+    # score = scoring_solution(synapse)
+    # print(f"score = {score}")
+    synapse_request = generate_problem()
+    synapse = solve(synapse_request)
+    print(f"tsp_tour = {synapse.solution}")
